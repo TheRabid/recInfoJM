9e0e4220dcb6b90c0197a5edb3847e217b96ea16
Alberto Sabater <asabater94@gmail.com>
2015-10-21 16:37:24 +0200

    End of search and spanish update

--- practica1/src/org/apache/lucene/demo/IndexFiles.java
+++ practica1/src/org/apache/lucene/demo/IndexFiles.java
@@ -18,6 +18,7 @@ package org.apache.lucene.demo;
  */
 
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.es.SpanishAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -98,7 +99,7 @@ public class IndexFiles {
 			System.out.println("Indexing to directory '" + indexPath + "'...");
 
 			Directory dir = FSDirectory.open(new File(indexPath));
-			Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_44);
+			Analyzer analyzer = new SpanishAnalyzer(Version.LUCENE_44);
 			IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_44, analyzer);
 
 			if (create) {
