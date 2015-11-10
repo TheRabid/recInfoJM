a1ee218f2d6606ff287989aeb09a430c0f968c91
Jaime <jai_mi_to@hotmail.es>
2015-11-10 15:23:57 +0100

    Prac3 Initial commit

---
+++ practica1/src/prac3/Evaluation.java
@@ -0,0 +1,42 @@
+package prac3;
+
+/**
+ * @author Alberto Sabater Bailon (546297)
+ * @author Jaime Ruiz-Borau Vizarraga (546751)
+ * 
+ *         La clase Evaluation hace [RELLENAR ESTO CUANDO TAL]
+ */
+
+public class Evaluation {
+
+	public static void main(String[] args) throws Exception {
+		/* Comprobar que se ha invocado bien al programa */
+		String usage = "Uso de este programa:\tjava prac3.Evaluation -qrels <qRelsFileName>"
+				+ " -results <resultsFileName> -output <outputFileName>";
+		if (args.length > 0 && ("-h".equals(args[0]) || "-help".equals(args[0]))) {
+			System.out.println(usage);
+			System.exit(0);
+		}
+
+		/* Variables por defecto */
+		String qrels = "qrels.txt";
+		String results = "results.txt";
+		String output = "output.txt";
+
+		/* Busqueda de los parametros del programa */
+		for (int i = 0; i < args.length; i++) {
+			if ("-qrels".equals(args[i])) {
+				qrels = args[i + 1];
+				i++;
+			} else if ("-results".equals(args[i])) {
+				results = args[i + 1];
+				i++;
+			} else if ("-output".equals(args[i])) {
+				output = args[i + 1];
+				i++;
+			}
+		}
+		
+		/* Funcionamiento del programa */
+	}
+}
