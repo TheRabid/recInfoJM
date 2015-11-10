5d0cc332a3ecc49324a4a72634398ac274d2ad49
Alberto Sabater <asabater94@gmail.com>
2015-11-10 15:40:35 +0100

    QrelFile AND ResultFile created

---
+++ practica1/src/prac3/QrelFile.java
@@ -0,0 +1,39 @@
+package prac3;
+
+public class QrelFile {
+
+	private int information_need;
+	private int document_id;
+	private int relevancy;
+
+	public int getInformation_need() {
+		return information_need;
+	}
+
+	public void setInformation_need(int information_need) {
+		this.information_need = information_need;
+	}
+
+	public int getDocument_id() {
+		return document_id;
+	}
+
+	public void setDocument_id(int document_id) {
+		this.document_id = document_id;
+	}
+
+	public int getRelevancy() {
+		return relevancy;
+	}
+
+	public void setRelevancy(int relevancy) {
+		this.relevancy = relevancy;
+	}
+
+	public QrelFile(int information_need, int document_id, int relevancy) {
+		this.information_need = information_need;
+		this.document_id = document_id;
+		this.relevancy = relevancy;
+	}
+
+}
