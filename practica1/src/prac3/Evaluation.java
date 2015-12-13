8b0cbb335ffdf0430f9d06e71b6897013cc53148
Jaime <jai_mi_to@hotmail.es>
2015-12-13 18:11:55 +0100

    minichange

--- practica1/src/prac3/Evaluation.java
+++ practica1/src/prac3/Evaluation.java
@@ -60,6 +60,9 @@ public class Evaluation {
 			System.out.printf("Precision: %.2f%n", getPrecision(v));
 			System.out.printf("Recall: %.2f%n", getRecall(v));
 			System.out.printf("F1 Score: %.2f%n", getF1Score(v));
+			
+			Values newV = updateNumbersK(10, i);
+			System.out.printf("Precision@10: %.2f%n", getPrecK(newV));
 			System.out.println(v.getTP());
 			System.out.println(v.getFP());
 			System.out.println(v.getFN());
@@ -99,8 +102,49 @@ public class Evaluation {
 
 		return new Values(infNeed, tp, fp, tn, fn);
 	}
+	
+	private static Values updateNumbersK(int k, int infNeed) {
+		int tp = 0;
+		int fp = 0;
+		int tn = 0;
+		int fn = 0;
+		int cuenta = 0;
+		for (QRel qrel : q) {
+			if(cuenta > k){
+				break;
+			}
+			if (infNeed == qrel.getInformation_need()) {
+				boolean found = false;
+				for (Result resul : r) {
+					if (infNeed == resul.getInformation_need() && qrel.getDocument_id() == resul.getDocument_id()) {
+						found = true;
+						if (qrel.getRelevancy() == 1)
+							tp++;
+						else
+							fp++;
+					}
+					if (found) {
+						break;
+					}
+				}
+				if (!found) {
+					if (qrel.getRelevancy() == 1)
+						fn++;
+					else
+						tn++;
+				}
+			}
+			cuenta++;
+		}
+
+		return new Values(infNeed, tp, fp, tn, fn);
+	}
 
 	private static double getPrecision(Values v) {
+		return v.getTP() / ((double) (v.getTP() + v.getFP()));
+	}
+	
+	private static double getPrecK(Values v) {
 		return v.getTP() / ((double) (v.getTP() + v.getFP()));
 	}
 
