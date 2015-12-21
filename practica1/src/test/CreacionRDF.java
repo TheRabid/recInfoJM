07f6f41651fadcf2eebdd722c02ba081e6bde4f4
Alberto Sabater <asabater94@gmail.com>
2015-12-21 16:19:52 +0100

    Part1

--- practica1/src/test/CreacionRDF.java
+++ practica1/src/test/CreacionRDF.java
@@ -20,6 +20,7 @@ package test;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.vocabulary.VCARD;
 
@@ -43,9 +44,31 @@ public class CreacionRDF {
         String givenName    = "John";
         String familyName   = "Smith";
         String fullName     = givenName + " " + familyName;
-
+        
+        String person2URI    = "http://somewhere/MikeJohnson";
+        String givenName2    = "Mike";
+        String familyName2   = "Johnson";
+        String fullName2     = givenName2 + " " + familyName2;
+        
+        String person3URI    = "http://somewhere/AliceMcTetis";
+        String givenName3    = "Alice";
+        String familyName3   = "McTetis";
+        String fullName3     = givenName3 + " " + familyName3;
+        
+        String propertyURI = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
+        String knowsURI = "http://xmlns.com/foaf/0.1/knows";
+        
+        String casaURI = "http://casa/Tarradellas";
+        String numHabitaciones = "4";
+        String comida = "fuet";
+        String tamaño = "Mu grande";
+        
+        
+        
         // crea un modelo vacio
         Model model = ModelFactory.createDefaultModel();
+        Property prop = model.createProperty(propertyURI);
+        Resource persona1 = model.createResource("http://xmlns.com/foaf/0.1/person");
 
         // le aÃ±ade las propiedades
         @SuppressWarnings("unused")
@@ -55,6 +78,36 @@ public class CreacionRDF {
                       model.createResource()
                            .addProperty(VCARD.Given, givenName)
                            .addProperty(VCARD.Family, familyName));
+        
+        johnSmith.addProperty(prop, persona1);
+        
+        
+        Resource mikeJohnson = model.createResource(person2URI)
+        		.addProperty(VCARD.FN, fullName2)
+                .addProperty(VCARD.N, 
+                         model.createResource()
+                              .addProperty(VCARD.Given, givenName2)
+                              .addProperty(VCARD.Family, familyName2));
+        		
+        Resource aliceMcTetis = model.createResource(person3URI)
+        		.addProperty(VCARD.FN, fullName3)
+                .addProperty(VCARD.N, 
+                         model.createResource()
+                              .addProperty(VCARD.Given, givenName3)
+                              .addProperty(VCARD.Family, familyName3));
+        
+        
+        Resource relacion = model.createResource("http://xmlns.com/foaf/0.1/knows")
+        		.addProperty(VCARD.FN, mikeJohnson)
+        		.addProperty(VCARD.FN, aliceMcTetis);
+        		
+        
+        Resource tarradellas = model.createResource(casaURI)
+        		.addProperty(VCARD.FN, numHabitaciones)
+        		.addProperty(VCARD.FN, tamaño)
+        		.addProperty(VCARD.FN, comida);
+        
+
         return model;
 	}
 	
