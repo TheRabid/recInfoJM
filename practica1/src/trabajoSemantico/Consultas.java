f24b2f443d0c2185108ca97332624f59e51bcc9d
Jaime <jai_mi_to@hotmail.es>
2016-02-07 19:37:25 +0100

    Update

--- practica1/src/trabajoSemantico/Consultas.java
+++ practica1/src/trabajoSemantico/Consultas.java
@@ -68,8 +68,9 @@ public class Consultas {
 			+ " SELECT DISTINCT ?id WHERE {"
 			+ "   ?doc recinfo:Identificador ?id."
 			+ "   ?doc recinfo:hasConcept ?concept."
-			+ "   { ?concept recinfo:conceptName \"videojuegos\".  "
-			+ "     ?concept skos:broader ?sub . "
+			+ "   { ?concept recinfo:conceptName \"videojuegos\". } "
+			+ "   UNION"
+			+ "   { ?concept skos:broader ?sub . "
 			+ "     ?sub recinfo:conceptName \"videojuegos\". } "
 			+ "   UNION"
 			+ "   { ?concept recinfo:conceptName \"diseño\" . "
@@ -78,7 +79,23 @@ public class Consultas {
 			+ "   FILTER (xsd:integer(?year) > 2010 && xsd:integer(?year) < 2016) "
 			+ " }";
 	
-	public static String c05_05 = "";
+	public static String c05_05 = ""
+			+ " PREFIX recinfo: <http://www.recInfo.com/> \n"
+			+ " PREFIX skos: <http://www.w3.org/TR/skos-primer/> \n"
+			+ " PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n"
+			+ " PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> \n"
+			+ " SELECT DISTINCT ?id WHERE {"
+			+ "   ?doc recinfo:Identificador ?id."
+			+ "   ?doc recinfo:hasConcept ?concept."
+			+ "   { ?concept recinfo:conceptName \"media\". } "
+			+ "   UNION"
+			+ "   { ?concept skos:broader ?sub . "
+			+ "     ?sub recinfo:conceptName \"arquitectura\". } "
+			+ "   UNION"
+			+ "   { ?concept recinfo:conceptName \"diseño\" . "
+			+ "     ?concept recinfo:conceptName \"personajes\". } "
+			+ "   ?doc recinfo:Fecha ?year"
+			+ "   FILTER (xsd:integer(?year) > 2010 && xsd:integer(?year) < 2016) ";
 	
 	public static String[] identificadores = {"13-2" , "02-4" , "09-3" , "07-2" , "05-5"};
 	public static String[] consultas = {c13_02 , c02_04, c09_03, c07_02, c05_05};
